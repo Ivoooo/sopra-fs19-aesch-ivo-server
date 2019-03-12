5ead2a97fd1ee9d03178c4f21aabea5d4d50d0e2
ivoooo <ivo.aeschlimann@hotmail.com>
2019-03-12 20:24:02 +0100

    Minor fixes in User class
    deleted unnecessary code
    added @JsonProperty so the password doesn't get sent to the client

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/User.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/User.java
@@ -1,28 +1,18 @@
 package ch.uzh.ifi.seal.soprafs19.entity;
 
 import ch.uzh.ifi.seal.soprafs19.constant.UserStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import java.util.Date;
 
 @Entity
 public class User implements Serializable {
-/*
-	public User(User user){
-		this.id = user.getId();
-		this.username = user.getUsername();
-		this.creationDate = user.getCreationDate();
-		this.status = user.getStatus();
-		this.password = user.getPassword();
-		this.token = user.getToken();
-	}
-*/
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -59,6 +49,7 @@ public class User implements Serializable {
 		return password;
 	}
 
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	public void setPassword(String name) {
 		this.password = name;
 	}
